4e04e0ed0205051979b6e23aa83e7c72e4aa98f2
rajanimohan22 <mohan.rao.dolla@digital.hmrc.gov.uk>
2019-02-22 11:51:26 +0000

    Coverage increased to 15.84

--- test/uniform/SdilComponents.scala
+++ test/uniform/SdilComponents.scala
@@ -16,16 +16,20 @@
 
 package uniform
 
+import java.time.LocalDate
+
 import ltbs.play.scaffold.SdilComponents
 import org.scalatest.{FlatSpec, Matchers}
 import play.api.data.Form
 import play.api.test.FakeRequest
 import sdil.models.Address
+import sdil.models.backend.{Site, UkAddress}
 import sdil.utils.TestWiring
 
 class SdilComponents extends FlatSpec with Matchers with TestWiring {
 
   val address : Address =  Address("line1", "line2", "line3", "line4", "AL1 1UJ")
+  val uKAddress : UkAddress = UkAddress(List("ukAddr1", "ukAddr2"), "WD25 7HQ")
   val requestContactDetails = FakeRequest().withFormUrlEncodedBody(
     "fullName" -> "Bill Gates",
     "position" -> "CEO retired",
@@ -33,14 +37,54 @@ class SdilComponents extends FlatSpec with Matchers with TestWiring {
     "email" -> "billgates@microsoft.com"
   )
 
-  val formContactDetails =  Form (
-    (SdilComponents.contactDetailsMapping))
+  val requestSiteDetails = FakeRequest().withFormUrlEncodedBody(
+    "address" -> uKAddress.toString,
+    "position" -> "CEO retired",
+    "phoneNumber" -> "No one knows",
+    "email" -> "billgates@microsoft.com"
+  )
 
+
+  val formContactDetails =  Form (SdilComponents.contactDetailsMapping)
+  val wareHouseSiteMapping = Form(SdilComponents.warehouseSiteMapping)
 
   "A SdilComponent Call for contactDetails" should "correctly bind" in {
 
     val addressHtmlVal = SdilComponents.addressHtml.showHtml(address)
     val contactDetailsVal = SdilComponents.contactDetailsForm.asHtmlForm("fail data", formContactDetails.bindFromRequest()(requestContactDetails))
+
+    val dateMap = Map("day" ->  "01",
+    "month" -> "02",
+    "year" -> "2019")
+
+    val siteObj = Site(uKAddress, Some("refOption"), Some("Arcade tradingName"), Some(LocalDate.now()))
+
+    val dayMissingMap = dateMap  + ("day" -> "")
+    val monthMissingMap = dateMap  + ("month" -> "")
+    val yearMissingMap = dateMap  + ("year" -> "")
+    val dayAndMonthMissingMap = dateMap  + ("day" -> "") + ("month" -> "")
+    val dayAndYearMissingMap = dateMap  + ("day" -> "") + ("year" -> "")
+    val monthAndYearMissingMap = dateMap  + ("month" -> "")  + ("year" -> "")
+    val allMissingMap = dateMap  + ("day" -> "") + ("month" -> "")  + ("year" -> "")
+    val invalidDayMap = dateMap  + ("day" -> "567")
+
+
+    SdilComponents.startDate.bind(dateMap).toString
+    SdilComponents.startDate.bind(dayMissingMap)
+    SdilComponents.startDate.bind(monthMissingMap)
+    SdilComponents.startDate.bind(yearMissingMap)
+    SdilComponents.startDate.bind(dayAndMonthMissingMap)
+    SdilComponents.startDate.bind(dayAndYearMissingMap)
+    SdilComponents.startDate.bind(monthAndYearMissingMap)
+    SdilComponents.startDate.bind(allMissingMap)
+    SdilComponents.startDate.bind(invalidDayMap)
+
+    SdilComponents.numeric("001")
+    SdilComponents.numeric("NotNumberic").bind(Map("dfgdf" -> "dfvdfv"))
+    SdilComponents.numeric("22.345")
+
+    SdilComponents.siteProgressiveRevealHtml.showHtml(siteObj).body
+
 
     1 shouldBe 1
 
